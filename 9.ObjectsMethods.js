6664e92b8878d0be77a07a092caf6f64f37238b3
MarisaTrigueros <159051163+MarisaTrigueros@users.noreply.github.com>
2024-05-03 13:31:57 +0200

    Add files via upload

---
+++ 9.ObjectsMethods.js
@@ -0,0 +1,16 @@
+const person = {
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 25
+}
+
+let keys = Object.keys(person);
+let values = Object.values(person);
+console.log(keys);
+console.log(values);
+
+
+// Otra manera: 
+// let information = Object.entries(person);
+
+// console.log(information);
